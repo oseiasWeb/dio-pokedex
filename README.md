10e685e4b3251a2d01bd85e13e8fb9a212140076
Oseias Nogueira <oseias.n.m@gmail.com>
2022-12-30 00:38:32 -0300

    Subindo projeto Pokedex e aplicando README

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center">Projeto Pokedex</h1>
+
+Este é um pequeno portfólio de apresentação das minhas habilidades como dev frontend. Ele tem uma apresentação pessoal, incluindo resumo breve sobre mim, redes sociais e lista de projetos. 💻📰✨
+
+<h2 align="center">*** Tecnologias Utilizadas ***</h2>
+<ul>
+    <li>HTML ✏️</li>
+    <li>CSS 🎨</li>
+    <li>JS 🧐</li>
+</ul>
+
+<h2 align="center">*** Como Utilizar ***</h2>
+<p>Existem 3 abas onde temos informações e links para contatos: Sobre, minhas redes e projetos. Abaixo temos um pequeno gif com as funcionalidades do sistema:</p>
+
+<img align="center" src="./src/imagens/animacao.gif" alt="Imagem Capa home" title="Tech Brasil"><p>Gif - funcionalidades do sistema</p>
+
+<h2 align="center">*** Link da página ***</h2>
+<p>Abaixo temos o link da página para visualização e testes:</p>
+🔗 <a href="https://oseiasweb.github.io/mini-portfolio/" target="_blank" class="linkedin">Mini Portfólio</a><br>
+
+<br>
+<h2 align="center">*** Status Projeto ***</h2>
+<p align="center">
+<img src="./assets/image/desenvolvimento.png"/>
+</p>
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,84 +1,15 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-function convertPokemonToLi(){
+function convertPokemonToLi(pokemon){
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Choque</li>
-                    <li class="type">Pequeno</li>
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
-                <img src="/assets/image/image_01.png" alt="${pokemon.name}">
-            </div>
-        </li>
-
-        <li class="pokemon">
-            <span class="number">#002</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">Fogo</li>
-                    <li class="type">Pequeno</li>
-                </ol>
-                <img src="/assets/image/image_02.png" alt="${pokemon.name}">
-            </div>
-        </li>
-
-        <li class="pokemon">
-            <span class="number">#003</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">Fogo</li>
-                    <li class="type">Grande</li>
-                </ol>
-                <img src="/assets/image/image_03.png" alt="${pokemon.name}">
-            </div>
-        </li>
-
-        <li class="pokemon">
-            <span class="number">#004</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">Água</li>
-                    <li class="type">Médio</li>
-                </ol>
-                <img src="/assets/image/image_04.png" alt="${pokemon.name}">
-            </div>
-        </li>
-
-        <li class="pokemon">
-            <span class="number">#005</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">Água</li>
-                    <li class="type">Pequeno</li>
-                </ol>
-                <img src="/assets/image/image_05.png" alt="${pokemon.name}">
-            </div>
-        </li>
-
-        <li class="pokemon">
-            <span class="number">#006</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    <li class="type">Mental</li>
-                    <li class="type">Pequeno</li>
-                </ol>
-                <img src="/assets/image/image_06.png" alt="${pokemon.name}">
+                <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -86,14 +17,7 @@ function convertPokemonToLi(){
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)    
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+}) 
